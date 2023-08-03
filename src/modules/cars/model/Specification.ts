379b17ad62ef5838f9136f3d553eae780c5c9ddf
GabrielVitorSilva <gabrieldatas2004@gmail.com>
2023-08-03 11:48:48 -0300

    feat: created specifications route

---
+++ src/modules/cars/model/Specification.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 as uuidv4 } from 'uuid';
+
+class Specification {
+  id?: string;
+  name: string;
+  description: string;
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { Specification };
